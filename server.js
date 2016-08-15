088d4631154415d17171920f6f776f7621d8a9f6
Amar Panchal <amar.panchal@gmail.com>
2016-08-14 17:59:21 -0700

    add socket.io

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ var express = require('express');
 var app = express();
 
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+io.on('connection', function() {
+	console.log('User connected via socket.io');
+});
+
 
 app.use(express.static(__dirname + '/public'));
 
